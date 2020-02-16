06195d227b0641b8944f382013bd7044d3cca815
Francisco Moreno Castillo <56913146+SakHyen@users.noreply.github.com>
2020-02-16 22:18:30 +0100

    Menu2

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,18 +1,23 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Text highscoreText;
+
+    void Start()	
     {
-        
+        highscoreText.text = "Highscore: " + PlayerPrefs.GetInt("highscore");
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Play()
     {
-        
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void CloseApp()
+    {
+        Application.Quit();
     }
 }
